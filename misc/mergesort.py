f5e5edb4c0e652e61d4eaf91c212dd16f1a92321
tkatzenbaer <me@katzenbaer.net>
2016-02-28 14:34:19 -0600

    Adds naive mergesort implementation

---
+++ misc/mergesort.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def mergesort(self, xs):
+        n = len(xs)
+        if n <= 1: return xs
+
+        pivot = n // 2
+        fs = self.mergesort(xs[:pivot])
+        ss = self.mergesort(xs[pivot:])
+
+        res = []
+        while len(fs) > 0 or len(ss) > 0:
+            if not fs:
+                res.append(ss.pop(0))
+            elif not ss:
+                res.append(fs.pop(0))
+            elif fs[0] < ss[0]:
+                res.append(fs.pop(0))
+            else:
+                res.append(ss.pop(0))
+        return res
+
+solution = Solution()
+
+res = solution.mergesort([6, 5, 3, 1, 8, 7, 2, 4])
+print res == [1, 2, 3, 4, 5, 6, 7, 8]
+
+res = solution.mergesort([8, 7, 6, 5, 4, 3, 2, 1])
+print res == [1, 2, 3, 4, 5, 6, 7, 8]
